694dcdfbc1c83f265de55855d44288195528c486
站在你的远方 <hello20110562@gmai.cm>
2014-12-07 20:25:25 +0800

    Revert "Account"
    This reverts commit f6c80860063eb8cf696817ff92b6672d6214d72d.

--- Adm.java
+++
@@ -1,9 +0,0 @@
-public class Adm{
-	public static void main(String [] args){
-		Account acc = new Account();//
-		acc.info();
-		acc.setPwd("154864");
-		acc.setUser("huang");
-		acc.info();
-	}
-}
--- Vistor.java
+++
@@ -1,6 +0,0 @@
-public class Vistor{
-		public static void main(String[] args){
-			Account acc = new Account();
-			System.out.println("user="+acc.user+"passworld="+acc.passworld );
-		}
-}
