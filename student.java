31e3f651bf1453df7f43970418619135e2e09003
站在你的远方 <hello20110562@gmai.cm>
2014-12-07 21:50:18 +0800

    Revert "Revert "h""
    This reverts commit 7aed6f75bcf61bd3e98260283eeea5df54a84ee0.

---
+++ student.java
@@ -0,0 +1,9 @@
+ public class student{
+	public static void main(String args[]){
+	System.out.println("*****************************");
+	System.out.println("|   1 add one student |");
+	System.out.println("|   2 exit the program       |");
+	System.out.println("| 3 please enter your choices|");
+	System.out.println("*****************************");
+	}
+	}
