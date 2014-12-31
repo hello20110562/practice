dfc692df5d3e843d9b739ae0fb2b83d3e83ef02b
hello20110562 <hello20110562@gmai.cm>
2014-12-31 23:17:24 +0800

    String

---
+++ 毕向东视频/String类/StringTest.java
@@ -0,0 +1,11 @@
+public class StringTest {
+		
+	public static  void main(String []args) {
+		char buff1 []={'s','d','3','d'};
+		String string=new String(buff1);
+		
+		String ss=string.valueOf('s');
+		System.out.println(ss);
+	 
+}
+}
